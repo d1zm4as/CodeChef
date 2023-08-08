57d676126eef92ea6094f104c5dc0a40ddbed30b
d1zm4as <pedrofamilia364@gmail.com>
2023-08-08 11:43:58 -0300

    +1

---
+++ BeginnerLevel/THREETOPICS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+
+	    int b,c,d,e;
+	    scanf("%d %d %d %d",&b ,&c,&d,&e);
+	    if(e==b || e==c ||e==d  ){
+	        printf("YES\n");
+	    }else{
+	        printf("NO\n");
+	    }
+	
+	return 0;
+}

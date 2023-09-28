7abdafab9425df773a0daaedc7462aa9e3b2241d
d1zm4as <pedrofamilia364@gmail.com>
2023-09-28 20:06:45 -0300

    +1

---
+++ BeginnerLevel/NEWSPAPER.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	
+	    int b;
+	    scanf("%d",&b);
+	    if(b>7){
+	        printf("YES\n");
+	    }else{
+	        printf("NO\n");
+	    }
+	
+	return 0;
+}

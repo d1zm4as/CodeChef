79669c0f8f721d521d9428ac62017dc056b7fb8e
d1zm4as <pedrofamilia364@gmail.com>
2023-11-06 09:16:29 -0300

    +1

---
+++ BeginnerLevel/CPRIVAL.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+
+	    int b,c;
+	    int d,e;
+	    scanf("%d %d",&b ,&c);
+	    scanf("%d %d",&d ,&e);
+	    if(b+d>c+e){
+	        printf("dominater\n");
+	    }else{
+	        printf("everule\n");
+	    }
+	
+	return 0;
+}

bfe9808219be4a3646f0125a398c9fcde92df742
d1zm4as <pedrofamilia364@gmail.com>
2024-03-06 18:35:39 -0300

    +2

---
+++ BeginnerLevel/HEALSE.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+
+	    int b;
+	    scanf("%d",&b );
+	    if(b<8){
+	        printf("less\n");
+	    }else if(b==8){
+	        printf("perfect\n");
+	    }else{
+	        printf("more");
+	    }
+	    
+	
+	return 0;
+}

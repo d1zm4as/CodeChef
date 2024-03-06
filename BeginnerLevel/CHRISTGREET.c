909fe410fff0e9779fb0b1cdb6f344e83c893318
d1zm4as <pedrofamilia364@gmail.com>
2024-03-06 18:21:14 -0300

    +3

---
+++ BeginnerLevel/CHRISTGREET.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	scanf("%d",&a);
+	
+    if(a==25){
+        printf("christmas\n");
+    }else{
+        printf("Ordinary\n");
+    }
+	
+	return 0;
+}
---
+++ BeginnerLevel/FOODCOST.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	
+	    int b,c;
+	    scanf("%d %d",&b ,&c);
+	    int total = (b*6)+c;
+	    printf("%d",total);
+	    
+	return 0;
+}
